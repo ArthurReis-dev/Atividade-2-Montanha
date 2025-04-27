df6ec84f1e559b275ce1b33fac4f0946685b91fb
Arthur Reis <arthurmendonca23@hotmail.com>
2025-04-27 12:25:47 -0300

    Primeiro

---
+++ Desafio/src/Desafio.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Desafio{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double p; //valor do investimento inicial
+        double r; //valor de juros anual
+        double t; //valor de investimento em anos
+
+        scanner.close();
+    }
+}
